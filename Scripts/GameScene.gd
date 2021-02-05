691362d91d0afff9168959734f20e5f8119ae37e
Edward Gorman <ejgorman@gmail.com>
2021-02-05 22:43:04 +0000

    Add more scene change controls

--- Scripts/GameScene.gd
+++ Scripts/GameScene.gd
@@ -1,5 +1,22 @@
 extends Node2D
 
+export(float, 16, 128) var MIN_X_DRAG_DISTANCE = 32
+onready var swipe_start = Vector2()
+
 func _on_Menu_button_down():
 	# Switch to Menu scene
 	return get_tree().change_scene("res://Scenes/Menu.tscn")
+
+func _on_MenuButtons_gui_input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		swipe_start = event.position
+	if event is InputEventScreenTouch and not event.pressed:
+		_calculate_swipe(event.position)
+
+func _calculate_swipe(swipe_end):
+	var swipe = swipe_end - swipe_start
+	if abs(swipe.x) > MIN_X_DRAG_DISTANCE:
+		if swipe.x > 0:
+			swipe_start = swipe_end
+		else:
+			_on_Menu_button_down()
--- Scripts/MenuScene.gd
+++ Scripts/MenuScene.gd
@@ -1,9 +1,29 @@
 extends Node2D
 
-func _on_StartGame_button_up():
+export(float, 16, 128) var MIN_X_DRAG_DISTANCE = 32
+onready var swipe_start = Vector2()
+
+func _on_StartGame_button_down():
 	# Switch to Game scene
 	return get_tree().change_scene("res://Scenes/Game.tscn")
 
 func _on_Settings_button_down():
 	# Switch to Settings scene
 	return get_tree().change_scene("res://Scenes/Settings.tscn")
+
+func _on_MenuButtons_gui_input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		swipe_start = event.position
+	if event is InputEventScreenTouch and not event.pressed:
+		_calculate_swipe(event.position)
+
+func _calculate_swipe(swipe_end):
+	var swipe = swipe_end - swipe_start
+	if abs(swipe.x) > MIN_X_DRAG_DISTANCE:
+		if swipe.x > 0:
+			_on_StartGame_button_down()
+		else:
+			_on_Settings_button_down()
+
+
+
